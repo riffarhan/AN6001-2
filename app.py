e07bee95d66bd73cffe050d607c8dfb8335d2958
Arif Farhan Bukhori <109029339+riffarhan@users.noreply.github.com>
2025-11-21 07:34:43 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
 
@@ -9,14 +10,29 @@ def index():
 @app.route("/main", methods=["GET", "POST"])
 def main():
     q = request.form.get("q")
-    print(q)
+    print("User name:", q)
     return render_template("main.html")
 
 @app.route("/dbs", methods=["GET", "POST"])
 def dbs():
     return render_template("dbs.html")
 
+@app.route("/dbs_prediction", methods=["GET", "POST"])
+def dbs_prediction():
+    q = request.form.get("q")
+    if q is None or q == "":
+        return "Error: Please enter a valid number."
+
+    q = float(q)
+
+    model = joblib.load("dbs.pkl")
+
+    r = model.predict([[q]])
+
+    result = float(r[0])
+
+    return render_template("dbs_prediction.html", prediction=result, rate=q)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
